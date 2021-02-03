107f0ab1f789727bfd5c9a4265ba1e49b9baeff1
Chiara M <48277192+chiaramichelucci@users.noreply.github.com>
2021-02-03 18:26:24 +0100

    Controller Recensione

---
+++ Controllers/addRecensione.php
@@ -0,0 +1,59 @@
+<?php
+require "../PDO/database.php";
+require "../Data/user.php";
+require "../Data/prodotto.php";
+require "../Data/recensione.php";
+require "../include/template2.inc.php";
+
+
+$failed = false;
+
+
+while (!$failed) {
+
+    if (isset($_POST['Titolo']) && !empty($_POST['Titolo'])) {
+        $titolo = $_POST['Titolo'];
+    }
+    else {
+        $msg = "Titolo mancante";
+        $failed = true;
+    }
+    if (isset($_POST['Voto']) && !empty($_POST['Voto'])) {
+        $voto = $_POST['Voto'];
+    }
+    else if (isset($_POST['Testo']) && !empty($_POST['Testo'])) {
+       $testo = $_POST['Testo'];
+    }
+    else {
+        $msg = "Testo mancante";
+        $failed = true;
+    }
+
+
+    $database = new Database();
+    $db = $database->getConnection(); 
+
+    $prodotto = new Prodotto($db);
+    $user = new User($db);
+    $addRec = new Recensione($db);
+
+    $id_prod = $prodotto->getByProdId($prodotto->id);
+    $id_user = $user->getByUserId($user->id);
+
+    $rec = $addRec->addRecens($id_user, $id_prod, $titolo, $voto, $testo);
+    $msg = "Recensione inserita correttamente!";
+
+        
+    if($failed = true){
+        callErrorTemplate($msg);
+    }
+
+        
+    function callErrorTemplate($errore){
+        $error = new Template("../dtml/error.html");
+        $error->setContent("msgError", $errore);
+        $error->close();
+    }
+
+
+?>
