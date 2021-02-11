0bfeef73c1117b73150385877d296a87ef18059d
alescare <45226635+alescare@users.noreply.github.com>
2021-02-11 15:56:25 +0100

    Update lottoPDO.php

--- PDO/lottoPDO.php
+++ PDO/lottoPDO.php
@@ -15,6 +15,22 @@
             $stmt->execute();
             return $stmt;
         }
+        
+        public function get($id_prod){
+            $sql = "SELECT * FROM " . $this->table_name . " WHERE id_prodotto = ? ORDER BY scadenza";
+            $stmt = $this->conn->prepare( $sql );
+            $stmt->execute([$id_prod]);
+            return $stmt;
+        }
+
+        
+        public function aggiornaQtaDisp($nuova_qta){
+            $sql = "UPDATE " . $this->table_name . " SET quantita_disponibile = ? WHERE id = " . $this->id;
+            $stmt = $this->conn->prepare( $sql );
+            $stmt->execute([$nuova_qta]);
+            return $stmt;
+        }
+        
     }
 
 ?>
