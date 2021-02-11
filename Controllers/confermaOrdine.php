ee32ed29b2fc953c13f6e2b73b3aeea3b8b32e48
alescare <45226635+alescare@users.noreply.github.com>
2021-02-11 16:01:35 +0100

    Update confermaOrdine.php

--- Controllers/confermaOrdine.php
+++ Controllers/confermaOrdine.php
@@ -10,6 +10,7 @@
     require "../Data/user.php";
     require '../Data/opzioneSpedizione.php';
     require '../Data/prodottoInOrdine.php';
+    require '../Data/lotto.php';
 
     session_start();
 
@@ -21,15 +22,16 @@
     $infoPag = new InfoPagamento($db);
     $ordine = new Ordine($db);
     $user = new User($db);
-    $opzPag = new opzionePagamento($db);
-    $prodINOrd= new prodottoInOrdine($db);
+    $opzPag = new OpzionePagamento($db);
+    $prodINOrd = new ProdottoInOrdine($db);
+    $lotto = new Lotto($db);
 
     //creazione ordine
     $ordine->stato = 'In preparazione';
     $ordine->data = date('Y/m/d-H:i:s');
     $ordine->id_user = $_SESSION['id_user'];
     $ordine->totale = $carrello->getTotale();
-    $ordineEffettuato = $ordine->creaOrdine($ordine->stato, $ordine->data, $ordine->id_user, $ordine->totale); //metodo che restituisce il risultato dell'inserimento come array (DA FARE)
+    $ordineEffettuato = $ordine->creaOrdine()->fetch(PDO::FETCH_ASSOC);; //risultato dell'inserimento come array 
     $id_ordine = $ordineEffettuato['id'];
 
     //verifica tipo di pagamento (alla consegna; carta; paypal) e creazione informazioni di pagamento per l'ordine
@@ -61,75 +63,104 @@
         
     } 
 
-    $infoPag->creaInfoPag($id_ordine, 
-                          $infoPag->$modalita, 
-                          $infoPag->$numero_carta, 
-                          $infoPag->$scadenza, 
-                          $infoPag->$cvv, 
-                          $infoPag->nome_proprietario); //da fare
+    $infoPag->creaInfoPag($id_ordine); 
+                           
     
     //eventuale aggiunta alle proprie carte
     if (isset($_POST['aggAlleMieCarte'])){
-        
-        $opzPag->creaOpzionePagamento($infoPag->numero_carta, 
-                                      $infoPag->scadenza, 
-                                      $infoPag->cvv, 
-                                      $nome_proprietario, 
-                                      $infoPag->nome_proprietario, 
-                                      $_SESSION['id_user']);
+
+        $opzPag->id_user = $_SESSION['id_user'];
+        $opzPag->tipologia = $infoPag->modalita;
+        $opzPag->numero_carta = $infoPag->numero_carta;
+        $opzPag->scadenza = $infoPag->scadenza;
+        $opzPag->cvv = $infoPag->cvv;
+        $opzPag->nome_proprietario = $infoPag->nome_proprietario;
+        $opzPag->aggiungiOpzionePagamento();
         
     }
 
     //creazione informazioni di spedizione
     if (isset($_POST['id_indirizzo'])){
 
-        $indirizzo = $opzSped->getById($_POST['id_indirizzo']);//da fare
-        $infoSped->creaInfoSped($id_ordine, 
-                                $indirizzo['citta'], 
-                                $indirizzo['via'], 
-                                $indirizzo['numero'], 
-                                $indirizzo['cap'], 
-                                $indirizzo['provincia'], 
-                                $indirizzo['paese'], 
-                                $indirizzo['altre_particolarita']);
+        $indirizzo = $opzSped->getOpzione($_POST['id_indirizzo'])->fetch(PDO::FETCH_ASSOC); // indirizzo relativo a $_POST['id_indirizzo']
+        $infoSped->citta = $indirizzo['citta']; 
+        $infoSped->via = $indirizzo['via']; 
+        $infoSped->numero = $indirizzo['numero'];
+        $infoSped->cap = $indirizzo['cap']; 
+        $infoSped->provincia = $indirizzo['provincia']; 
+        $infoSped->paese = $indirizzo['paese']; 
+        $infoSped->altre_particolarita = $indirizzo['altre_particolarita'];
+        $infoSped->creaInfoSped($id_ordine);
 
     } else {
 
-        $infoSped->creaInfoSped($id_ordine, 
-                                $_POST['citta'], 
-                                $_POST['via'], 
-                                $_POST['numero'], 
-                                $_POST['cap'], 
-                                $_POST['provincia'], 
-                                $_POST['paese'], 
-                                $_POST['altre_particolarita']);
+        $infoSped->citta = $_POST['citta']; 
+        $infoSped->via = $_POST['via']; 
+        $infoSped->numero = $_POST['numero']; 
+        $infoSped->cap = $_POST['cap']; 
+        $infoSped->provincia = $_POST['provincia']; 
+        $infoSped->paese = $_POST['paese']; 
+        $infoSped->altre_particolarita = $_POST['altre_particolarita'];
+        $infoSped->creaInfoSped($id_ordine); 
         
         if(isset($_POST['aggAiMieiInd'])){
 
-            $opzSped->creaOpzSped($_POST['citta'],
-                                  $_POST['via'],
-                                  $_POST['numero'], 
-                                  $_POST['cap'], 
-                                  $_POST['provincia'], 
-                                  $_POST['paese'], 
-                                  $_POST['altre_particolarita'], 
-                                  $_SESSION['id_user']);
+            $opzSped->citta = $_POST['citta'];
+            $opzSped->via = $_POST['via'];
+            $opzSped->numero = $_POST['numero']; 
+            $opzSped->cap = $_POST['cap']; 
+            $opzSped->paese = $_POST['provincia']; 
+            $opzSped->paese = $_POST['paese']; 
+            $opzSped->altre_particolarita = $_POST['altre_particolarita']; 
+            $opzSped->id_user = $_SESSION['id_user'];
+            $opzSped->aggiungiOpzioneSpedizione();
 
         }
     } 
     
     //creazione prodotti in ordine
-    $prodotti = $carrello->getByUserID($_SESSION['id_user']);//lista prodotti nel carrello 
+    $prodotti = $carrello->viewCart($_SESSION['id_user']);//lista prodotti nel carrello dell'utente
     $cont = 0;
     while ($cont < $prodotti->rowCount()){
 
-        $prodotto = $prodotti->fetch(PDO::FETCH_ASSOC); //iterazione sui prodotti
-        $prodINOrd->aggProdInOrd($id_ordine, $prodotto['id_prodotto'], $prodotto['quantita']);
-        $cont++;
+        $prodotto = $prodotti->fetch(PDO::FETCH_ASSOC); //iterazione sui prodotti nel carrello (contiene il prodotto attuale)
+        //$prodINOrd prende i dati del prodotto del carrello 
+        $prodINOrd->id_ordine = $id_ordine;
+        $prodINOrd->id_prodotto = $prodotto['id_prodotto'];
+        $lotti_prod = $lotto->get($prodotto['id_prodotto']);//lotti relativi al prodotto (in ordine ascendente rispetto alla data di scadenza)
+        
+        //ciclo che crea i prodotti in ordine scalando la loro quantità da quella dei relativi lotti
+        while ($prodotto['quantita'] != 0){
+
+            $lotto_att = $lotti_prod->fetch(PDO::FETCH_ASSOC); //lotto attuale
+            if ($lotto_att['quantita_disponibile'] >= $prodotto['quantita']){ //la quantità del lotto attua
+
+                $lotto_att['quantita_disponibile'] -= $prodotto['quantita'];
+                //aggiornamento lotto
+                $lotto->id = $lotto_att['id'];
+                $lotto->aggiornaQtaDisp( $lotto_att['quantita_disponibile']);
+                //crea prodotto in ordine con i dati del lotto attuale
+                $prodINOrd->quantita = $prodotto['quantita'];
+                $prodINOrd->id_lotto = $lotto_att['id'];
+                $prodINOrd->creaProdInOrd();
+                $prodotto['quantita'] = 0;
+
+            } else {
+
+                $prodINOrd->quantita = $lotto_att['quantita_disponibile'];
+                $prodINOrd->id_lotto = $lotto_att['id'];
+                $prodINOrd->creaProdInOrd();
+                $prodotto['quantita'] -= $lotto_att['quantita_disponibile'];
+                $lotto_att['quantita_disponibile'] = 0;
+                $lotto->aggiornaQtaDisp( $lotto_att['quantita_disponibile']);
+
+            }
+            
+        }
         
     } 
 
-    $carrello->svuotaCarrello();//da fare
+    $carrello->svuotaCarrello($_SESSION['id_user']);
     $main = new Template("../dtml/loggedUserMain.html");
     $body = new Template("../dtml/riepilogoOrdine.html");
     //main e body set content
@@ -137,4 +168,3 @@
     $main->close();
 
 ?>
-
